836f0120843fcdcee7873248735439c7203130fd
nuuuwan <nuuuwan@gmail.com>
2026-02-03 15:09:01 +0530

    Refactor GoogleMapsUtils methods for improved structure and readability

--- src/googlemaps_utils/GoogleMapsUtils.py
+++ src/googlemaps_utils/GoogleMapsUtils.py
@@ -6,19 +6,20 @@ import polyline
 
 class GoogleMapsUtils:
     @staticmethod
-    def get_route_latlng_list(
-        route_num: str,
-        start_location_name: str,
-        end_location_name: str,
-    ) -> list[tuple]:
+    def __get_gmaps_client() -> googlemaps.Client:
         api_key = os.environ.get("GMAPS_API_KEY")
         if not api_key:
             raise ValueError(
                 "GOOGLE_MAPS_API_KEY environment variable not set"
             )
+        return googlemaps.Client(key=api_key)
 
-        gmaps = googlemaps.Client(key=api_key)
-
+    @staticmethod
+    def __get_directions(
+        gmaps: googlemaps.Client,
+        start_location_name: str,
+        end_location_name: str,
+    ) -> list[dict]:
         directions_result = gmaps.directions(
             origin=start_location_name,
             destination=end_location_name,
@@ -32,7 +33,12 @@ class GoogleMapsUtils:
                 + f" {start_location_name} to {end_location_name}"
             )
 
-        selected_route = None
+        return directions_result
+
+    @staticmethod
+    def __find_matching_route(
+        directions_result: list[dict], route_num: str
+    ) -> dict:
         for route in directions_result:
             for leg in route.get("legs", []):
                 for step in leg.get("steps", []):
@@ -40,16 +46,26 @@ class GoogleMapsUtils:
                     line = transit_details.get("line", {})
                     short_name = line.get("short_name", "")
                     if short_name == route_num:
-                        selected_route = route
-                        break
-                if selected_route:
-                    break
-            if selected_route:
-                break
+                        return route
 
-        if not selected_route:
-            selected_route = directions_result[0]
+        return directions_result[0]
 
-        encoded_polyline = selected_route["overview_polyline"]["points"]
-        decoded_latlng_list = polyline.decode(encoded_polyline)
-        return decoded_latlng_list
+    @staticmethod
+    def __decode_route_polyline(route: dict) -> list[tuple]:
+        encoded_polyline = route["overview_polyline"]["points"]
+        return polyline.decode(encoded_polyline)
+
+    @staticmethod
+    def get_route_latlng_list(
+        route_num: str,
+        start_location_name: str,
+        end_location_name: str,
+    ) -> list[tuple]:
+        gmaps = GoogleMapsUtils.__get_gmaps_client()
+        directions_result = GoogleMapsUtils.__get_directions(
+            gmaps, start_location_name, end_location_name
+        )
+        selected_route = GoogleMapsUtils.__find_matching_route(
+            directions_result, route_num
+        )
+        return GoogleMapsUtils.__decode_route_polyline(selected_route)
